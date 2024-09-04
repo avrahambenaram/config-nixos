8ba4f20a92d2c27b62211d6883f8d4d1dfa937ef
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-09-04 13:19:38 -0300

    chore: remove kde gtk config

--- config/displays.nix
+++ config/displays.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   xdg.portal.wlr.enable = true;
@@ -6,8 +6,13 @@
   programs.xwayland.enable = true;
   programs.sway.enable = true;
   services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs; [
+    kdePackages.kde-gtk-config
+  ];
 
   # Display manager
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
 }
