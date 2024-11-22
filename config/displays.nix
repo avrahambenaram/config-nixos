adf2072ae1ed552537a6d9c2d88e1416fd7e1182
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-11-22 10:11:20 -0300

    chore: use sddm in xorg

--- config/displays.nix
+++ config/displays.nix
@@ -1,18 +1,13 @@
-{ pkgs, ... }:
-
 {
   xdg.portal.wlr.enable = true;
 
   programs.xwayland.enable = true;
   programs.sway.enable = true;
   services.desktopManager.plasma6.enable = true;
-  environment.plasma6.excludePackages = with pkgs; [
-    kdePackages.kde-gtk-config
-  ];
 
   # Display manager
+  services.xserver.enable = true;
   services.displayManager.sddm = {
     enable = true;
-    wayland.enable = true;
   };
 }
